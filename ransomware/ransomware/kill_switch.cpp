951667444c1ea0215cb7b73dd41795a329fa13bb
ryancor <ryan_cornateanu@yahoo.com>
2018-08-30 15:11:44 -0700

    edit on TODO list

--- ransomware/ransomware/kill_switch.cpp
+++ ransomware/ransomware/kill_switch.cpp
@@ -5,5 +5,6 @@ vector<wstring> decrypt_files(wstring path)
 	vector<wstring> subdirs, matches;
 	// TODO: finish the decrypting of files
 	// TODO: Must reset ACLs before files can be opened again.
+	// TODO: Replace file extensions to a signature .txt->.w4nnacry
 	return matches;
 }
