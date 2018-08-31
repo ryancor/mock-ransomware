fa2d49a8d1fded360031d1b4aa1b24f8c57808e9
ryancor <ryan_cornateanu@yahoo.com>
2018-08-31 12:21:11 -0700

    notes on how killswitch should work

--- ransomware/ransomware/kill_switch.cpp
+++ ransomware/ransomware/kill_switch.cpp
@@ -6,5 +6,6 @@ vector<wstring> decrypt_files(wstring path)
 	// TODO: finish the decrypting of files
 	// TODO: Must reset ACLs before files can be opened again.
 	// TODO: Replace file extensions to a signature .txt->.w4nnacry
+	// TODO: Delete persistance in registry keys
 	return matches;
 }
